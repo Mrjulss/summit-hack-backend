65499329d9255b71e1a985d27507c176be62d8d8
Christoph <christoph.thees@gmx.de>
2025-03-20 13:37:16 +0100

    CRM

---
+++ summit_hack_backend/crm.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+
+class UserCRMInfo:
+    def __init__(self, user_file, stock_file):
+        self.users = pd.read_csv(user_file)
+        self.stocks = pd.read_csv(stock_file)
+
+    def get_user_details(self, user_id):
+        user = self.users[self.users['UserID'] == user_id]
+        if user.empty:
+            return f"User with ID {user_id} not found."
+        return user.to_dict(orient='records')[0]
+
+    def get_user_stocks(self, user_id):
+        user_stocks = self.stocks[self.stocks['UserID'] == user_id]
+        if user_stocks.empty:
+            return f"No stock information found for User ID {user_id}."
+        return user_stocks.to_dict(orient='records')
+
+    def get_user_id(self, user_name):
+        user = self.users[self.users['UserName'] == user_name]
+        if user.empty:
+            return f"User with name {user_name} not found."
+        return user['UserID'].values[0]
+
+
+user_stock_info = UserCRMInfo("../CSV_Users.csv", "../data.csv")
+
+user_name = "Christoph Thees"
+user_id = user_stock_info.get_user_id(user_name)
+
+print("User Details:", user_stock_info.get_user_details(user_id))
+print("User Stocks:", user_stock_info.get_user_stocks(user_id))
--- summit_hack_backend/widgets/customer.py
+++ summit_hack_backend/widgets/customer.py
@@ -1,9 +1,8 @@
-
 class CustomerContent:
-    def __init__(self, name: str, age: int, risk_aversity: int, location: str, profession: str, source_of_wealth: str):
+    def __init__(self, name: str, age: int, risk_aversion: int, location: str, profession: str, source_of_wealth: str):
         self.name = name
         self.age = age
-        self.risk_aversity = risk_aversity
+        self.risk_aversion = risk_aversion
         self.location = location
         self.profession = profession
         self.source_of_wealth = source_of_wealth
