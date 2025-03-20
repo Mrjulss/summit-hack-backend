6fc6b8c0d6bf7dbfa9ca77b95f63dc0ba1789065
go49los <go49los@mytum.de>
2025-03-20 13:48:05 +0100

    prompt template for chatgpt

--- summit_hack_backend/chat_gpt.py
+++ summit_hack_backend/chat_gpt.py
@@ -7,7 +7,33 @@ def ask_chat_gpt(prompt, model="gpt-4"):
 
     response = client.chat.completions.create(
         model=model,
-        messages=[{"role": "system", "content": "You are a helpful assistant."},
+        messages=[{"role": "system", "content": """
+                You will receive a prompt related to wealth management. Your task is to break it down into multiple specific queries suitable for a stateless third-party API. Each query should be self-contained, including all necessary parameters especially the users assets and shares. Additionally, select the most suitable widget type to display the results. Answer only in the JSON format provided in the example.
+
+                Widget Types: news, timeseries, kpi, customer
+                
+                Some interesting KPIs to use in queries:
+                market capitalization, price to earnings, earnings to share, dividend yield, performance, volatility, sharpe ratio, beta...
+                
+                Example:
+                 
+                Prompt: "Why is Teslas stock price developing so badly?"
+                
+                Your Answer: [
+                {
+                "clean_query": "What are current events regarding tesla",
+                "widget": "news" 
+                },
+                {
+                "clean_query":  "How did the tesla stock develop over the last year?",
+                "widget": "timeseries"
+                },
+                {
+                "clean_query":  "What is the volatility of the tesla stock",
+                "widget": "kpi"
+                }
+                ]
+        """},
                   {"role": "user", "content": prompt}],
         temperature=0.7,
         max_tokens=200,
--- summit_hack_backend/widget_chooser.py
+++ summit_hack_backend/widget_chooser.py
@@ -1,3 +1,6 @@
+import json
+from summit_hack_backend.prompt_templates.prompt import Prompt
+
 
 def select_widgets(query: str):
     # call llm endpoint to get list of widgets
@@ -5,3 +8,7 @@ def select_widgets(query: str):
     # return list of widgets to display
     return ["news", "timeseries", "kpi", "customer"]
 
+
+def parse_json_to_queries(json_string: str) -> list[Prompt]:
+    prompt_list = [Prompt(**item) for item in json.loads(json_string)]
+    return prompt_list
